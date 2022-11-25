bb70c9e7a59085f42b102b8dd2ade5c8cbe99d63
Dominik Schmeiser <dominik@schmeiser.eu>
2022-11-25 13:49:22 +0100

    updated samples to php 8.1

--- samples/consolesample/consolesample.php
+++ samples/consolesample/consolesample.php
@@ -1,16 +1,28 @@
 <?php
 
+use ConfigCat\ClientOptions;
+use ConfigCat\ConfigCatClient;
+use ConfigCat\Log\LogLevel;
+use ConfigCat\User;
+use Monolog\Logger;
+
 require __DIR__ . '/vendor/autoload.php';
 
-$client = new \ConfigCat\ConfigCatClient("PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ", [
-    \ConfigCat\ClientOptions::LOGGER => new \Monolog\Logger("consolesample"),
-    // Info level logging helps to inspect the feature flag evaluation process.
-    // Use the default Warning level to avoid too detailed logging in your application.
-    \ConfigCat\ClientOptions::LOG_LEVEL => \ConfigCat\Log\LogLevel::INFO,
-]);
+$client = new ConfigCatClient(
+    'PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ',
+    [
+        ClientOptions::LOGGER => new Logger('consolesample'),
+        // Info level logging helps to inspect the feature flag evaluation process.
+        // Use the default Warning level to avoid too detailed logging in your application.
+        ClientOptions::LOG_LEVEL => LogLevel::INFO,
+    ]
+);
 
-$user = new \ConfigCat\User("Some UserID",
-    "configcat@example.com", "Awesomnia", [ "version" => "1.0.0" ]
+$user = new User(
+    'Some UserID',
+    'configcat@example.com',
+    'Awesomnia',
+    [ 'version' => '1.0.0' ]
 );
 
 $value = $client->getValue('isPOCFeatureEnabled', false, $user);
